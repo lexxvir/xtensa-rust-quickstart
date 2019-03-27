d3edbeda4432f6d379b6117806eeda4d00823754
Alexey Arbuzov <aarbuzov@termt.com>
2019-03-27 10:49:10 +0300

    Use alloc & allocator

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,32 @@
 #![no_std]
 #![no_main]
+#![feature(alloc)]
+#![feature(alloc_error_handler)]
+
+extern crate alloc;
 
 use core::panic::PanicInfo;
 use core::sync::atomic::{self, Ordering};
 
 use panic_ramdump as _;
+
+use core::alloc::{GlobalAlloc, Layout};
+use core::ptr::null_mut;
+
+struct MyAllocator;
+
+unsafe impl GlobalAlloc for MyAllocator {
+    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 { null_mut() }
+    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
+}
+
+#[alloc_error_handler]
+fn foo(_: core::alloc::Layout) -> ! {
+    panic!()
+}
+
+#[global_allocator]
+static A: MyAllocator = MyAllocator;
 
 #[no_mangle]
 fn main() {
